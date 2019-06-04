cae7c6efe0bb1d3ecec50c96f0f2c5de4cdc313a
aydentko <ayumi_aruku@live.jp>
2019-06-04 22:03:37 +0900

    UnityFile

---
+++ Assets/CreateBlocks.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBlocks : MonoBehaviour {
+    public BlockData[] blocks_arg;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OrderBlocks()
+    {
+
+    }
+}
+
+public enum BlocksColor
+{
+    RED, BLUE, GREEN, YELLOW
+}
+
+public enum BlocksState
+{
+    IDLE, CHANGE, NONE
+}
+
+public class BlockData
+{
+    BlocksColor color;
+    BlocksState state;
+    Vector2Int position;
+
+
+}
+
+
